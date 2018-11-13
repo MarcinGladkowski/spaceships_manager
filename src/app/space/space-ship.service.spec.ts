489158ddd72b336cb43ec178b92f53fb914c9cee
Gladkowski <marcingladkowski@gmail.com>
2018-11-13 22:19:23 +0100

    Create test for produce on fighter ship

--- src/app/space/space-ship.service.spec.ts
+++ src/app/space/space-ship.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
+import { FighterShip } from './fighter-ship';
+import { SpaceShipType } from './space-ship-type.enum';
+import { HttpClient } from '@angular/common/http';
 import { SpaceShipService } from './space-ship.service';
+import { HttpClientMock } from './pilot.service.spec';
+import { OrderFormValue } from './order-form-value';
+import { map } from 'rxjs/operators';
+import { SpaceShip } from './space-ship';
 
-describe('SpaceShipService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: SpaceShipService = TestBed.get(SpaceShipService);
-    expect(service).toBeTruthy();
+fdescribe('SpaceShipService', () => {
+
+  let spaceShipService: SpaceShipService;
+  let http: HttpClient;
+
+  beforeEach(() => {
+    http = new HttpClientMock();
+    spaceShipService = new SpaceShipService();
   });
+
+  describe('produceShips', () => {
+
+    beforeEach(() => {
+        const orderFormValue: OrderFormValue = {shipCount: 2, shipType: SpaceShipType.Fighter};
+        spaceShipService.produceShips(orderFormValue);
+    });
+
+    it('should hangar has one ship', () => {
+      spaceShipService.hangarShips.pipe(map((ships) =>
+        expect(ships.length).toEqual(1)
+        )
+      );
+    });
+
+    it('should hangar has one fighter ship', () => {
+      spaceShipService.hangarShips.pipe(map((ship) => {
+        expect(ship instanceof SpaceShip).toBeTruthy();
+        expect(ship instanceof FighterShip).toBeTruthy();
+      }));
+    });
+
+  });
+
+
+
 });
